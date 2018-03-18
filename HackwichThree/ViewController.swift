696461d2d4ba5c60fb1f7a9f28a68f32d2062e19
Scott A <salquisa@hawaii.edu>
2018-03-18 07:46:42 -1000

    Fixed text in about m tab

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet var menuTitleOne: UINavigationItem!
-    
+    @IBOutlet var myNameIsLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var myMajorIsLabel: UILabel!
+    @IBOutlet var majorLabel: UILabel!
+    @IBOutlet var aboutMeLabel: UILabel!
+    @IBOutlet var aboutMeTextView: UITextView!
     
     
     override func viewDidLoad() {
@@ -22,6 +27,12 @@ class ViewController: UIViewController {
     
    self.firstLabel.text = "About"
         self.menuTitleOne.title = "About Me"
+        self.myNameIsLabel.text = "My name is"
+        self.nameLabel.text = "Scott Alquisa"
+        self.myMajorIsLabel.text = "My major is"
+        self.majorLabel.text = "Creative Media"
+        self.aboutMeLabel.text = "About Me"
+        self.aboutMeTextView.text = "I am a sophomore at University of Hawaiʻi at West Oahu. I am taking five courses for the Spring 2018 semester. I am taking CM 120, CM 161, PHIL 102, CHEM 100, and ICS 101."
     }
 
     override func didReceiveMemoryWarning() {
