c949f42bae97d54715058a130c9832f1d235f418
Scott A <salquisa@hawaii.edu>
2018-03-17 23:38:31 -1000

    Fixed label in weather tab Fixed outlet connections in about me and weather tab. App "crashes" on bucket list tab. Having trouble finding whats wrong with the bucket list tab.

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet var menuTitleOne: UINavigationItem!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
