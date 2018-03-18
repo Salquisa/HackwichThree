c5f27c2c2abf835d89984b0a18202d909c9fd7fb
Scott A <salquisa@hawaii.edu>
2018-03-17 20:21:33 -1000

    Worked on tab 3

--- HackwichThree/ViewControllerThree.swift
+++ HackwichThree/ViewControllerThree.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class ViewControllerThree: UIViewController {
-
+class ViewControllerThree: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    @IBOutlet var menuTitleThree: UINavigationItem!
+    @IBOutlet var tableView: UITableView!
+    var bucketListArray = ["Visit Japan", "Take pictures at the Urban Lights in Los Angeles", "Go on a roadtrip", "Go camping in a National Park", "Visit Paris with loved one"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ self.menuTitleThree.title = "My Bucket List"
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +27,22 @@ class ViewControllerThree: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        bucketListArray.count
+        }
+
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+            UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+                let text = bucketListArray[indexPath.row]
+                cell.textLabel?.text = text
+                return cell
+                
+        }
 
     /*
     // MARK: - Navigation
@@ -32,4 +54,5 @@ class ViewControllerThree: UIViewController {
     }
     */
 
+}
 }
